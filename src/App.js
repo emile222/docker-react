a15ec961fa2da30d07d55064024d84c51e3f1267
Emile Amewoto <eamewoto@fnb.co.za>
2020-01-15 15:58:29 +0200

    modify app.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         Hi there!
+        Learn react: Hi there!
         </a>
       </header>
     </div>
